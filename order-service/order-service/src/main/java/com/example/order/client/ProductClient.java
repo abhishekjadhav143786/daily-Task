1d2105d5618dad90776c16bdc1f3a57c18187ea9
abhishek <abhishekj143786@gmail.com>
2025-12-12 12:31:21 +0530

    updated

--- order-service/order-service/src/main/java/com/example/order/client/ProductClient.java
+++ order-service/order-service/src/main/java/com/example/order/client/ProductClient.java
@@ -5,15 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// Connects to Product Service on Port 8081
+
 @FeignClient(name = "product-service")
 public interface ProductClient {
     @GetMapping("/products/{id}")
     ProductDTO getProductById(@PathVariable("id") Long id);
 }
-
-
-
-
-
-
--- order-service/order-service/src/main/java/com/example/order/client/UserClient.java
+++ order-service/order-service/src/main/java/com/example/order/client/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// Connects to User Service on Port 8082
+
 @FeignClient(name = "user-service")
 public interface UserClient {
     @GetMapping("/users/{id}")
